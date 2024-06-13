6eb7eb5e269a87fe1103755cb51809fcdd65b4ae
Jay Rathod <76587844+jayrathod3210@users.noreply.github.com>
2024-06-13 14:18:41 +0530

    Update Logauth_Session.cy.js

--- Logauth_Session.cy.js
+++ Logauth_Session.cy.js
@@ -1,40 +1,24 @@
 
 beforeEach(() => {
+  
+  cy.session("login",()=>{  // Sesssion started
+  cy.visit('URL') // Add your visit URL
+  // Perform action for select login.
 
-
-  cy.session("login",()=>{
-
-  cy.visit('http://uat.service365.com/signin')
-  cy.get('#cookie-acceptance-button').click()
-  cy.get('#app-sign-in-button').click()
-  cy.wait(2000)
-
-
-    cy.origin("https://login.microsoftonline.com/", () => {
-      cy.get('[name=loginfmt]').type("Jay.rathod@service365.com")
+    cy.origin("https://login.microsoftonline.com/", () => {   // Add your origin e.g google,microsoft,etc.
+    // Find locator to enter login and password field 
+    cy.get('[name=loginfmt]').type("",{log:false})  // Enter email.
     cy.wait(2000)
     cy.get('#idSIButton9').click();  // next button
-    cy.get('#i0118').type("tatvasoft@3210")
+    cy.get('#i0118').type("",{log:false}) // Enter password
     cy.get('#idSIButton9').should('be.visible').click({ force: true })
-    //cy.get('#app-sign-in-button').click({ force: true })
-  
-  
-  });
     
-  cy.visit("http://uat.service365.com/")
-  cy.get('#app-sign-in-button').click({ force: true })
-  
-  cy.get('#timesheetTile > .card-enabled').click({force: true})
-
+  });  
+  cy.visit("http://")  // Enter your URL again.
 })
 });
 
-it("Timesheet", ()=>{
-  cy.visit("http://uat.service365.com/")
-  //cy.get('#app-sign-in-button').click({ force: true })
-  cy.get('#timesheetTile > .card-enabled').click({force: true})
-
+it("Test", ()=>{
+  
+// Write you code.
 })
-
- 
-
